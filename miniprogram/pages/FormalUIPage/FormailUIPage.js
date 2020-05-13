2b09776dff7a287f737577bcfd286bd185fa50a8
黄继辉 <17373407@buaa.edu.cn>
2020-05-13 21:26:10 +0800

    finish #3

---
+++ miniprogram/pages/FormalUIPage/FormailUIPage.js
@@ -0,0 +1,129 @@
+// miniprogram/pages/FormalUIPage/FormailUIPage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    icon: [{ name: 'appreciate', isShow: true }, { name: 'check', isShow: true }, { name: 'close', isShow: true }, { name: 'edit', isShow: true }, { name: 'emoji', isShow: true }, { name: 'favorfill', isShow: true }, { name: 'favor', isShow: true }],
+    TabCur : 0,
+    scrollLeft:0,
+    checkbox: [{
+      value: 0,
+      name: '10元',
+      checked: false,
+      hot: false,
+    }, {
+      value: 1,
+      name: '20元',
+      checked: true,
+      hot: false,
+    }, {
+      value: 2,
+      name: '30元',
+      checked: true,
+      hot: true,
+    }, {
+      value: 3,
+      name: '60元',
+      checked: false,
+      hot: true,
+    }, {
+      value: 4,
+      name: '80元',
+      checked: false,
+      hot: false,
+    }, {
+      value: 5,
+      name: '100元',
+      checked: false,
+      hot: false,
+    }]
+  },
+
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id-1)*60
+    })
+  },
+
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  ChooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let values = e.currentTarget.dataset.value;
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      if (items[i].value == values) {
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+    this.setData({
+      checkbox: items
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
